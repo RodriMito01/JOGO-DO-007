3ed95f01ce48617a7bbf19ac54c2575565015a51
RodriMito01 <61715945+RodriMito01@users.noreply.github.com>
2020-03-04 11:39:57 -0300

    Add files via upload

---
+++ GAME002.py
@@ -0,0 +1,47 @@
+print('---Jogo DO 007!---')
+from random import randint
+from time import sleep
+itens = ('DEFESA', 'ATAQUE', 'CARREGAR')
+Bot = randint(0, 2)
+print('''Suas jogadas são:
+[0] DEFESA
+[1] ATAQUE
+[2] CARREGAR''')
+Player = int(input('Qual sua jogada: '))
+print('0')
+sleep(1)
+print('0')
+sleep(1)
+print('7!!!!')
+print('=-=-' * 10)
+print('O jogador escolheu: {}'.format(itens[Player]))
+print('Bot escolheu: {}'.format(itens[Bot]))
+print('=-=-' * 10)
+sleep(2)
+if Bot == 0:  # DEFESA
+    if Player == 0:
+        print('EMPATE')
+    elif Player == 1:
+        print('JOGO CONTINUA')
+    elif Player == 2:
+        print('BOT SALVO')
+    else:
+        print('JOGADA INVÁLIDA')
+elif Bot == 1:  # ATAQUE
+    if Player == 0:
+        print('JOGO CONTINUA')
+    elif Player == 1:
+        print('EMPATE')
+    elif Player == 2:
+        print('BOT VENCE')
+    else:
+        print('JOGADA INVÁLIDA')
+elif Bot == 2:  # CARREGAR
+    if Player == 0:
+        print('JOGO CONTINUA')
+    elif Player == 1:
+        print('PLAYER VENCE')
+    elif Player == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA')
